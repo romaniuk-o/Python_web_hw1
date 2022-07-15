13f99910387b6ae770b27041f890be5a92c8bc9e
romaniuk-o <O.romaniuk54@gmail.com>
2022-07-15 11:37:30 +0300

    update

--- web_hw_1_2.py
+++ web_hw_1_2.py
@@ -2,13 +2,15 @@ class Meta(type):
     class_number = 0
 
     def __new__(mcs, name, bases, namespace, **kwargs):
+        print(f'__new__ Meta {mcs.class_number}')
         instance = super().__new__(mcs, name, bases, namespace, **kwargs)
         instance.class_number = mcs.class_number
         mcs.class_number += 1
+        mcs.children_number = mcs.class_number
         return instance
 
 
-Meta.children_number = 5
+Meta.children_number = 0
 
 
 class Cls1(metaclass=Meta):
@@ -25,3 +27,4 @@ if __name__ == '__main__':
     assert (Cls1.class_number, Cls2.class_number) == (0, 1)
     a, b = Cls1(''), Cls2('')
     assert (a.class_number, b.class_number) == (0, 1)
+    print(Meta.children_number)
